cbf3b1d3bc77ccd456ef875a49b0e3381227b51d
Tony Camaiani <camaiani@gmail.com>
2015-01-24 16:34:01 +0100

    moving tracking function to main.js

--- js/main.js
+++ js/main.js
@@ -5,5 +5,18 @@ $(function() {
 	 minFont   : 15,
 	 maxFont   : 40,
 	 fontRatio : 50
-	}); 
+	});
+
+	$(document).ready(function(){
+        // set google analytics onclick link event on each link with class track
+        $('a.track').each(function(index, element){
+            element = $(element);
+            var link = element.attr('href');
+            var new_window = element.attr('target') == '_blank' ? true : false;
+            element.click(function(){
+            trackOutboundLink(link, new_window);
+            return false;
+            });
+        });
+    });
 });
